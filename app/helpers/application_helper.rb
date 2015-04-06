687b2157a59a18c289f8b7375b88448679e6ca52
nkopa <natalia16k@wp.pl>
2015-04-06 16:50:00 +0000

    podstawowe strony

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def displayTitle(string = "")
+       base_title = "Movie Share" 
+       if string.blank?
+           base_title
+       else
+           "#{string} | #{base_title}"
+       end
+    end
 end
